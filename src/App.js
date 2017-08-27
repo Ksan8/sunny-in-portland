7e7a23fdf680a44b61112ef766b0c5241b298148
Kezia Wineberg <ksan8@protonmail.com>
2017-08-27 15:00:41 -0700

    Comment code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     const API_KEY = '053e6d728d7629123c0e73d87e6d7d68';
     const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?id=5746545&units=imperial&mode=json&APPID=';
     let url = BASE_URL + API_KEY;
-
+    // AJAX request to OpenWeatherMap
     let request = $.get(url);
 
     request.done(result => {
@@ -28,33 +28,32 @@ export default class App extends Component {
         result.list[23],
         result.list[31],
       ];
-      // console.log(nextFive);
-
+      // Create inididual divs for each day
       const weatherList = nextFive.map(function(day, index){
+        // Convert date from Unix; display only day of week, month, and date
         let date = new Date(day.dt * 1000).toString().split(' ');
         let displayDate = date[0] + ' ' + date[1] + ' ' + date[2];
-        console.log(date[0]);
 
-        let key = index;
+        let key = index; // React requires a unique key for any mapped items
         let temp = day.main.temp;
         let max = day.main.temp_max;
         let min = day.main.temp_min;
         let weather = day.weather[0].description;
         let icon = 'http://openweathermap.org/img/w/' + day.weather[0].icon + '.png';
 
-        return <div key={key} className='weather-box'>
+        return <section key={key}>
           <img key={key} src={icon} alt={weather}></img>
           <div className='temp-box'>
             <h4 key={key + 'date'}>{displayDate}</h4>
             <h1 key={key + 'temp'}>{temp}°F</h1>
             <h4 key={key + 'max'}>Max: {max}°F</h4>
             <h4 key={key + 'min'}>Min: {min}°F</h4>
-            <p key={key + 'we'}>{weather}</p>
+            <p key={key + 'weather'}>{weather}</p>
           </div>
-        </div>
+        </section>
         ;
       });
-
+      // Store only data to be used in state
       this.setState({
         data: weatherList,
         show: !this.state.show
